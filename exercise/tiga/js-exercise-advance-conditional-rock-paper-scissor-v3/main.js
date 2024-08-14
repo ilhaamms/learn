6a21622d8414abb57dda6a7f97a64a5c65b9eef6
Ilham Muhammad Sidiq <ilhaammuhammads@gmail.com>
2024-08-15 00:19:54 +0700

    until excercise 3

---
+++ exercise/tiga/js-exercise-advance-conditional-rock-paper-scissor-v3/main.js
@@ -0,0 +1,36 @@
+function rockPaperScissor(player1, player2) {
+
+  if (player1 === "paper" && player2 === "paper" || player1 === "rock" && player2 === "rock" ||
+    player1 === "scissor" && player2 === "scissor") {
+    return `Draw!`
+  }
+
+  if (player1 === "paper" || player2 === "paper" && player1 === "rock" || player2 === "rock") {
+    if (player1 === "paper") {
+      return `Player 1 Won!`
+    } else {
+      return `Player 2 Won!`
+    }
+  } else if (player1 === "rock" || player2 === "rock" && player1 === "scissor" || player2 === "scissor") {
+    if (player1 === "rock") {
+      return `Player 1 Won!`
+    } else {
+      return `Player 2 Won!`
+    }
+  } else if (player1 === "scissor" || player2 === "scissor" && player1 === "paper" || player2 === "paper") {
+    if (player1 === "scissor") {
+      return `Player 1 Won!`
+    } else {
+      return `Player 2 Won!`
+    }
+  }
+}
+
+console.log(rockPaperScissor('scissor', 'paper')); // "Player 1 Won!"
+console.log(rockPaperScissor('rock', 'paper')); // "Player 2 Won!"
+console.log(rockPaperScissor('paper', 'paper')); // "Draw!"
+console.log(rockPaperScissor('rock', 'rock')); // "Draw!"
+console.log(rockPaperScissor('paper', 'rock')); // "Draw!"
+console.log(rockPaperScissor('scissor', 'rock')); // "Draw!"
+
+module.exports = rockPaperScissor;
